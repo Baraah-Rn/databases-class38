925c357137fa30fe3be2e85af205cb620b9f9eaa
Baraah-Rn <baraahranneh@gmail.com>
2022-09-13 10:24:17 +0200

    adding transaction file

---
+++ Week3/homework/transaction/transaction.js
@@ -0,0 +1,54 @@
+import mysql from "mysql";
+import util from "util";
+import { createTables } from "./createTables.js";
+import { insertData } from "./insertValues.js";
+
+const CONNECTION_CONFIG = {
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+};
+
+const SEND_AMOUNT = `
+  UPDATE account
+  SET balance = balance - 1000
+  WHERE account_number = 101;
+`;
+
+const RECEIPT_AMOUNT = `
+  UPDATE account
+  SET balance = balance + 1000
+  WHERE account_number = 102;
+`;
+
+const transfer = [SEND_AMOUNT, RECEIPT_AMOUNT];
+const createDb = [
+  "DROP DATABASE IF EXISTS wallet;",
+  "CREATE DATABASE wallet;",
+  "USE wallet;",
+];
+
+const executeDB = async () => {
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const execQuery = util.promisify(connection.query.bind(connection));
+  createDb.map((query) => execQuery(query));
+  console.log("database is created");
+  createTables.map((query) => execQuery(query));
+  console.log("tables are created");
+  insertData.map((query) => execQuery(query));
+  console.log("data is inserted");
+  connection.beginTransaction();
+  try {
+    const transaction = transfer.map((query) => execQuery(query));
+    await Promise.all(transaction);
+    connection.commit();
+    console.log("transaction done");
+    connection.end();
+  } catch (err) {
+    connection.rollback();
+    connection.end();
+    console.log(err);
+  }
+};
+
+executeDB();
